4764977525ec2a2e19d43771d8c5a00af06ecb32
Yidong <yidong@iappsasia.com>
2022-05-19 15:59:09 +0800

    init

---
+++ src/B2BService.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Iapps\Ihg\Rpc;
+
+
+class B2BService
+{
+    public function call()
+    {
+        return 'hello world';
+    }
+
+}
---
+++ src/Rpc.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Iapps\Ihg\Rpc;
+
+
+class Rpc
+{
+    public function call()
+    {
+        return 'hello world';
+    }
+
+}
