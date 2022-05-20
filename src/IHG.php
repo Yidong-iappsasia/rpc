4ca192c31f381efdcb8e7b4910f679341eaa7d98
Yidong <yidong@iappsasia.com>
2022-05-20 18:09:26 +0800

    w

--- src/IHG.php
+++ src/IHG.php
@@ -55,6 +55,48 @@ class IHG
         return null;
     }
 
+    public static function curl($url, $method = '', $header = [], $data = [], $referer = '', $cookie = '', $gzip = false, $returnCookie = 0)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36');
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
+        curl_setopt($curl, CURLOPT_REFERER, $referer);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
 
+        if ($method == "POST") {
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        }
 
+        if ($cookie) {
+            curl_setopt($curl, CURLOPT_COOKIE, $cookie);
+        }
+
+        if ($header) {
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+        }
+
+        if ($gzip) {
+            curl_setopt($curl, CURLOPT_ENCODING, 'gzip');
+        }
+
+        curl_setopt($curl, CURLOPT_HEADER, $returnCookie);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $data = curl_exec($curl);
+
+        if (curl_errno($curl)) {
+            throw new \Exception(curl_error($curl));
+        }
+
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+
+        return [
+            'http_code' => $httpCode,
+            'body'      => $data
+        ];
+    }
 }
--- src/Service.php
+++ src/Service.php
@@ -10,6 +10,9 @@ class Service
     public $name;
     public $address;
 
+    private $class;
+    private $action;
+
     public function __construct($name)
     {
 
@@ -17,21 +20,65 @@ class Service
 
     public function __get($name)
     {
-        print_r($name);
+        $this->class = $name;
         return $this;
     }
 
     public function __call($name, $arguments)
     {
-        print_r([$name, $arguments]);
+        $this->action = $name;
+        $route        = '/rpc';
 
-        return $this;
+        $params = [
+            'class'  => $this->class,
+            'action' => $this->action,
+            'params' => $arguments
+        ];
+
+        return $this->call($route, $params);
         // ihg_rpc_client
     }
 
     public function call($route, $params, $method = 'POST', $headers = [])
     {
+        $getAllHeaders = function () {
+            $headers = [];
+            if (is_array($_SERVER)) {
+                foreach ($_SERVER as $name => $value) {
+                    if (substr($name, 0, 5) == 'HTTP_') {
+                        $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                    }
+                }
+            }
+            return $headers;
+        };
+        $allHeaders    = $getAllHeaders();
+        if (is_array($headers) && $headers) {
+            foreach ($headers as $key => $header) {
+                $allHeaders[$key] = $header;
+            }
+        }
 
+        if (empty($this->address[0])) {
+            throw new \Exception($this->name . ' service not registered.', '404');
+        }
+
+        $index = rand(0, count($this->address) - 1);
+        $url   = $this->address[$index];
+        $url   = rtrim($url, '/');
+        $route = ltrim($route, '/');
+        $url   = $url . '/' . $route;
+
+        try {
+            $response = IHG::curl($url, $method, $allHeaders, $params);
+            if ($response['http_code'] == 200) {
+                return $response['body'];
+            } else {
+                throw new \Exception($response['body'], $response['http_code']);
+            }
+        } catch (\Exception $exception) {
+            throw  $exception;
+        }
     }
 
 }
